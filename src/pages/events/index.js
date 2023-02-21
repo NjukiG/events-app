5266d4ae8fd12eed200de8a04278b4ae5fa509e7
George <gnjuki19@gmail.com>
2023-02-21 15:30:47 +0300

    Rendered the events page woth data from the json server

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,26 +1,30 @@
-import React from "react";
+import Image from "next/image";
 
-function EventsPage() {
+function EventsPage({ data }) {
+  // console.log(data)
   return (
     <div>
       <h1>Events Page</h1>
-      <a href="">
-        <img />
-        <h2>Events in Nairobi</h2>
-      </a>
 
-      <a href="">
-        <img />
-        <h2>Events in Mombasa</h2>
-      </a>
-
-      <a href="">
-        <img />
-        <h2>Events in Kisumu</h2>
-      </a>
+      {data.map((event) => (
+        <a key={event.id} href={`/events/${event.id}`}>
+          <Image src={event.image} alt={event.title} width={400} height={300} />
+          <h2>{event.title}</h2>
+        </a>
+      ))}
     </div>
   );
 }
 
 export default EventsPage;
 
+export async function getStaticProps() {
+  const { events_categories } = await import("/data/data.json");
+  // console.log(events_categories)
+
+  return {
+    props: {
+      data: events_categories,
+    },
+  };
+}
