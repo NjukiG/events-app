b6487ee4bf6ed8cce7aab1216b958578dce8d71b
George <gnjuki19@gmail.com>
2023-02-23 12:30:16 +0300

    The api post request is working and new emails can now be added to the list of registered atendees

--- src/pages/api/email-registration.js
+++ src/pages/api/email-registration.js
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 
 function buildPath() {
-  return path.join(process.cwd, "data", "data.json");
+  return path.join(process.cwd(), "data", "data.json");
 }
 
 function extractData(filePath) {
@@ -24,11 +24,25 @@ export default function handler(req, res) {
   if (method === "POST") {
     const { email, eventId } = req.body;
 
-    const newAllEvents = allEvents.map((event) =>{
-        if(event.id === eventId){
-            
+    const newAllEvents = allEvents.map((event) => {
+      if (event.id === eventId) {
+        if (event.emails_registered.includes(email)) {
+          res
+            .status(201)
+            .json({ message: "THis email is already registered!" });
         }
-    })
+        return {
+          ...event,
+          emails_registered: [...event.emails_registered, email],
+        };
+      }
+      return event;
+    });
+
+    fs.writeFileSync(
+      filePath,
+      JSON.stringify({ events_categories, allEvents: newAllEvents })
+    );
 
     res.status(200).json({
       message: `You have been registered successfully with the email: ${email} to the event ${eventId}`,
