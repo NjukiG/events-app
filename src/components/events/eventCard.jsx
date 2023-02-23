66fd11526e167bf11eab62db8a0a21e4eaae0d7d
George <gnjuki19@gmail.com>
2023-02-23 12:08:48 +0300

    Setting up the post request while learning some express js too. Coding along.

--- src/components/events/eventCard.jsx
+++ src/components/events/eventCard.jsx
@@ -4,14 +4,34 @@ import Image from "next/image";
 
 export const EventCard = ({ data }) => {
   const inputEmail = useRef();
-  console.log(inputEmail);
+  // console.log(inputEmail);
   const router = useRouter();
-  console.log(router)
+  // console.log(router)
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+    // console.log(eventId)
+
+    try {
+      const responce = await fetch("/api/email-registration", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: emailValue, eventId: eventId }),
+      });
+
+      if (!responce.ok) {
+        throw new Error(`Error: ${responce.status}`);
+      }
+      const data = await responce.json();
+
+      console.log("POST", data)
+    } catch (e) {}
   };
+
   return (
     <div className="event_single_page">
       <h1>{data.title}</h1>
--- src/pages/api/email-registration.js
+++ src/pages/api/email-registration.js
@@ -1,3 +1,37 @@
+import path from "path";
+import fs from "fs";
+
+function buildPath() {
+  return path.join(process.cwd, "data", "data.json");
+}
+
+function extractData(filePath) {
+  const jsonData = fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
 export default function handler(req, res) {
-    res.status(200).json({"name": "George Ako hapa msee!"})
+  const { method } = req;
+
+  const filePath = buildPath();
+  const { events_categories, allEvents } = extractData(filePath);
+
+  if (!allEvents) {
+    return res.status(404).json({ message: "Events data not found!" });
+  }
+
+  if (method === "POST") {
+    const { email, eventId } = req.body;
+
+    const newAllEvents = allEvents.map((event) =>{
+        if(event.id === eventId){
+            
+        }
+    })
+
+    res.status(200).json({
+      message: `You have been registered successfully with the email: ${email} to the event ${eventId}`,
+    });
+  }
 }
